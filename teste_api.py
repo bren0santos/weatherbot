d5e12f984941c5dd7a2d111b5dbe41c3db12ae3d
Breno Santos <santosbreno112@gmail.com>
2026-09-14 12:35:04 -0300

    add: nova função de buscar localizacao

--- teste_api.py
+++ teste_api.py
@@ -1,25 +1,34 @@
 import requests
 
-url = "https://geocoding-api.open-meteo.com/v1/search"
-params = {
-    "name": "Campinas",
-    "count": 1,
-    "language": "pt",
-    "format": "json",
-}
-try:
-    resposta = requests.get(url, params=params, timeout=10)
-    resposta.raise_for_status()
-    
-    print(resposta.status_code)
-    
-    dados = resposta.json()
-    
-    print(dados["results"][0]["name"])
-    print(dados["results"][0]["latitude"])
-    print(dados["results"][0]["longitude"])
-    
-except (requests.exceptions.Timeout, requests.exceptions.HTTPError):
-    print("A requisição excedeu o tempo limite")
+def buscar_localizacao(cidade):
+    url = "https://geocoding-api.open-meteo.com/v1/search"
+    params = {
+        "name": cidade,
+        "count": 1,
+        "language": "pt",
+        "format": "json",
+    }
+    try:
+        resposta = requests.get(url, params=params, timeout=10)
+        resposta.raise_for_status()
+               
+        dados = resposta.json()
 
+        if "results" in dados and dados["results"]:  
+            localizacao = {
+                "name": dados["results"][0]["name"],
+                "latitude": dados["results"][0]["latitude"],
+                "longitude": dados["results"][0]["longitude"]
+            }
+            return localizacao
+        else:
+            return None
+        
+    except requests.exceptions.Timeout:
+        print("A requisição excedeu o tempo limite")
+    except requests.exceptions.HTTPError:
+        print("A API retornou um erro HTTP.")
+
+resultado = buscar_localizacao("Campinas")
+print(resultado)
 
