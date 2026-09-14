8c16a0a142ec54e2d1fd505675edc97fe0cdecd3
Breno Santos <santosbreno112@gmail.com>
2026-09-14 19:41:35 -0300

    feat: add weather response formatting

--- main.py
+++ main.py
@@ -22,7 +22,37 @@ if cidade is not None:
         latitude = localizacao["latitude"]
         longitude = localizacao["longitude"]
         clima = buscar_clima(latitude, longitude)
-        print(clima)
 else:
     print("Mensagem não reconhecida!")
     
+def interpretar_weather_code(codigo):
+    if codigo == 0:
+        return "céu limpo"
+    elif 1 <= codigo <= 3:
+        return "diferentes condições de céu nublado"
+    elif 45 <= codigo <= 48:
+        return "neblina"
+    elif 51 <= codigo <= 57:
+        return "garoa"
+    elif 61 <= codigo <= 67:
+        return "chuva"
+    elif 71 <= codigo <= 77:
+        return "neve"
+    elif 80 <= codigo <= 82:
+        return "pancadas de chuva"
+    elif 95 <= codigo <= 99:
+        return "tempestade"
+    else:
+        return "código desconhecido"
+
+def resposta_usuario(cidade, clima):
+        
+    temperatura = clima["temperature_2m"]
+    velocidade_vento = clima["wind_speed_10m"]
+    codigo = clima["weather_code"]
+    
+    condicao_climatica = interpretar_weather_code(codigo)
+    
+    return f'O clima em {cidade} está com {str(temperatura).replace(".",",")}°C. \nCondição: {condicao_climatica}.\nVento: {str(velocidade_vento).replace(".",",")} km/h'
+
+print(resposta_usuario(cidade, clima))
